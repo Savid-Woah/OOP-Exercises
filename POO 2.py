d8810cd43e5bdd11fa5e5b6ad53807dee514f28a
Savid-Woah <108183046+Savid-Woah@users.noreply.github.com>
2022-06-25 09:31:07 -0500

    Add files via upload

---
+++ POO 2.py
@@ -0,0 +1,29 @@
+class user():
+
+    def __init__(self, name, age, salary, debts):
+        
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.debts = debts
+
+    # def user_data(name):    
+
+    def verify(self):
+
+        if self.age > 30 and self.salary > 2 and self.debts < (self.salary*0.3):
+
+            print("Hi " + self.name + " we have verified your data and we are glad to say that you are elegible for a credit")
+
+        else: print("Hi " + self.name + " we are sorry to say you can't get a credit here :(") 
+
+
+name = input("Type your name here ---> ")
+age = int(input("How old are you ---> "))
+salary = int(input("How much do you earn per month ---> "))
+debts = int(input("What's your debt ---> "))
+
+person = user(name, age, salary, debts)
+
+person.verify()
+
